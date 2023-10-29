985536082219d74bc454b5faf6b14a6f7628480f
Sethu Narayan Senthil <github@sethusenthil.com>
2023-10-29 20:12:46 +0000

    [LeetCode Sync] Runtime - 133 ms (15.27%), Memory - 50.5 MB (52.04%)

--- my-folder/0001-two-sum/solution.js
+++ my-folder/0001-two-sum/solution.js
@@ -3,13 +3,28 @@
  * @param {number} target
  * @return {number[]}
  */
-function twoSum(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i + 1; j < nums.length; j++) {
-            if(nums[i] + nums[j] === target) {
-                return [i, j];
+var twoSum = function(nums, target) {
+    let resArr = null;
+
+    for(let i = 0; i < nums.length; i++){
+        const thisNum = nums[i];
+        
+        const neededNum = target - thisNum;
+        
+        console.log('needednum', neededNum)
+
+        for(let j = i + 1; j < nums.length; j++){
+            if(neededNum === nums[j]){
+                resArr = [i, j];
+                break;
             }
         }
+        
+        if(resArr != null){
+        	break;
+        }
     }
-}
 
+    return resArr;
+    
+};
